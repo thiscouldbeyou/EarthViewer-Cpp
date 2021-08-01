d1d5c2c460f1b69b77b3179aeeec48e19812f17a
Tristan <tristanascott123@gmail.com>
2021-07-31 22:55:25 -0500

    Removed build files

--- build/debug/Scene/Scenes/Scene.d
+++
@@ -1,8 +0,0 @@
-build/debug/Scene/Scenes/Scene.o: src/Scene/Scenes/Scene.cpp \
- src/Scene/Scenes/Scene.hpp src/EVSystem.hpp src/Events/Event.hpp
-
-src/Scene/Scenes/Scene.hpp:
-
-src/EVSystem.hpp:
-
-src/Events/Event.hpp:
--- build/debug/Utilities/GeneralMath.d
+++
@@ -1,4 +0,0 @@
-build/debug/Utilities/GeneralMath.o: src/Utilities/GeneralMath.cpp \
- src/Utilities/GeneralMath.hpp
-
-src/Utilities/GeneralMath.hpp:
